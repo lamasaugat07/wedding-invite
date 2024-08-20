5c6a0647f21fe876288d8000f5f7d3059878de43
lamalab <lamasaugat07@gmail.com>
2024-08-20 19:48:01 -0400

    Form validation, new pictures

--- js/script.js
+++ js/script.js
@@ -10,6 +10,47 @@ offCanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
 
+document.getElementById('name').onchange = function(){
+  if (this.value.trim() == "") {
+    this.classList.remove('valid');
+    this.classList.add('invalid');
+  } else {
+    this.classList.remove('invalid');
+    this.classList.add('valid');
+  }
+}
+
+document.getElementById('email').onchange = function(){
+  var validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (this.value.trim().toLowerCase() == "" || !this.value.trim().toLowerCase().match(validEmailRegex)) {
+    this.classList.remove('valid');
+    this.classList.add('invalid');
+  } else {
+    this.classList.remove('invalid');
+    this.classList.add('valid');
+  }
+}
+
+document.getElementById('status').onchange = function(){
+  if (this.value === "Choose one") {
+    this.classList.remove('valid');
+    this.classList.add('invalid');
+  } else {
+    this.classList.remove('invalid');
+    this.classList.add('valid');
+  }
+}
+
+document.getElementById('attend').onchange = function(){
+  if (this.value === "" || this.value < 0) {
+    this.classList.remove('valid');
+    this.classList.add('invalid');
+  } else {
+    this.classList.remove('invalid');
+    this.classList.add('valid');
+  }
+}
+
 function showCustomAlert(message) {
   var modal = document.getElementById("customAlert");
   var messageElem = document.getElementById("alertMessage");
@@ -42,15 +83,17 @@ window.addEventListener("load", function () {
     
     const data = new FormData(form);
     var jsonData = {};
+    var validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     var name = document.getElementById('name').value.trim();
+    var email = document.getElementById('email').value.trim().toLowerCase();
     var attend = document.getElementById('attend').value;
     var status = document.getElementById('status').value;
 
-    if (name === "" || attend === "" || status === "Choose one") {
+    if (name === "" || email == "" || !email.match(validEmailRegex) || attend === "" || status === "Choose one") {
       showCustomAlert("Please fill out all fields before submitting.");
     } else {
       data.forEach((value, key) => jsonData[key] = value);
-      const action = 'https://script.google.com/macros/s/AKfycbzyJvd38PR3ywm0KiYr55LmafqcRlX7nJID8UQFHxpohVfx5dLzaPeR2OkeHmn8oJ4/exec';
+      const action = 'https://script.google.com/macros/s/AKfycbw70phajQ7vX8cfkL1BPu97PbYv4hK86leWv5AGHUznDEG1rqUp3SzieRqX8m74BhB3/exec';
       fetch(action, {
         method: "POST",
         body: JSON.stringify(jsonData),  
